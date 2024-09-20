f927be160a3f01739bf2a87bf3d07e64b1e564b5
Renan Mayrinck <renan.mtk@gmail.com>
2024-09-20 03:16:21 -0300

    atualizações na página inicial

--- js/interactions.js
+++ js/interactions.js
@@ -3,6 +3,15 @@ const ContactAbort = document.getElementById("clear");
 const ContactDialog = document.getElementById("contact-form");
 const CloseTrigger = document.getElementById("close");
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 ContactTrigger.addEventListener("click", () => {
     ContactDialog.showModal();
