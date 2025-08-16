c56f40094b57022da08d93c1ef09e6f6b3cc4457
mayrinck <renan.mtk@gmail.com>
2025-08-16 05:28:36 -0300

    alteração das datas dos posts

--- js/share.js
+++ js/share.js
@@ -2,19 +2,19 @@
 document.getElementById("share").addEventListener("click", async () => {
     const tituloArtigo = document.querySelector("h1")?.textContent.trim() || "este artigo";
     const textoCompartilhamento = `Acabei de ler este artigo: ${tituloArtigo}, no site do @RenanMayrinck`;
-
+    
     if (navigator.share) {
-    try {
-        await navigator.share({
-        title: document.title,
-        text: textoCompartilhamento,
-        url: window.location.href
-        });
-        console.log("Compartilhamento realizado com sucesso!");
-    } catch (error) {
-        console.error("Erro ao compartilhar:", error);
-    }
+        try {
+            await navigator.share({
+            title: document.title,
+            text: textoCompartilhamento,
+            url: window.location.href
+            });
+            console.log("Compartilhamento realizado com sucesso!");
+        } catch (error) {
+            console.error("Erro ao compartilhar:", error);
+        }
     } else {
-    alert("O compartilhamento nativo não é suportado neste dispositivo/navegador, mas você pode copiar o link no botão ao lado");
+        alert("O compartilhamento nativo não é suportado neste dispositivo/navegador, mas você pode copiar o link no botão ao lado");
     }
 });
